4223b1d42519f0d37be243fce6c6f4824089a507
jon2502 <jonbjarke25@outlook.dk>
2025-08-19 20:52:27 +0200

    testing env integration of node version 20.6.0+

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const test = process.env.TEST
+
+console.log(test)
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 

64dac46ba169df12ccc4b7b2388a958299d8f47e
jon2502 <jonbjarke25@outlook.dk>
2025-08-20 10:52:45 +0200

    updated schema with Subdocuments

--- models/Decks.js
+++ models/Decks.js
@@ -1,23 +1,22 @@
 const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
-    name: {
-        type:String,
-        required: true,
-    },
-    format: {
-        type:String,
-        required: true,
-    },
-    cards: [
-        {
-            name: String, required: true,
-            count: Number, required: true,
-            set: String, required: true,
-            setid: Number, required: true
-        },
-        
-    ],
+  name: {
+    type:String,
+    required: true,
+  },
+  format: {
+    type:String,
+    required: true,
+  },
+  deck: {
+    type: [categorySchema],
+    required: true
+  }, 
+  legality: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type:Date,
     required: true,
@@ -27,5 +26,19 @@ const schema = mongoose.Schema({
     required: true,
   }
 })
+
+const categorySchema = new mongoose.Schema({
+  categoryName: {type:String, default: 'Main',},
+  cards: [cardSchema]
+},{ _id: false })
+
+const cardSchema = new mongoose.Schema({
+  cardname: { type: String, required: true },
+  count: { type: Number, required: true },
+  set: { type: String, required: true },
+  setid: { type: Number, required: true }
+},{ _id: false })
+
+
 //mongoose.model("name of collection the model is for", the schema to use for the model);
 module.exports = mongoose.model("Decks", schema);
